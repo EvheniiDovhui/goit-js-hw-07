67ce50da137de52b56257684fb0e5687d3b13f85
EvheniiDovhui <edovguy97@gmail.com>
2023-11-12 11:31:32 +0200

    script

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryItem(item) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = item.original;
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = item.preview;
+  image.setAttribute("data-source", item.original);
+  image.alt = item.description;
+
+  link.link;
+  appendChild(image);
+  listItem.appendChild(link);
+
+  galleryList.appendChild(listItem);
+
+  // Add click event listener to open the modal
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const instance = basicLightbox.create(
+      `<img src="${item.original}" alt="${item.description}">`
+    );
+    instance.show();
+  });
+}
+
+galleryItems.forEach(createGalleryItem);
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    const lightboxCloseButton = document.querySelector(
+      ".basicLightbox__wrapper"
+    );
+    if (lightboxCloseButton) {
+      lightboxCloseButton.click();
+    }
+  }
+});
